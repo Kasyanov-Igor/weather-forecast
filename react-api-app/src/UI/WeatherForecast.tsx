07e349a3472b2f5155b2ed440f739ab5e1c096f0
Igor <161635179+Kasyanov-Igor@users.noreply.github.com>
2025-12-11 20:41:03 +0300

    Add css style

--- react-api-app/src/UI/WeatherForecast.tsx
+++ react-api-app/src/UI/WeatherForecast.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/purity */
 import React from 'react';
 import type { Weather } from '../Model/weather';
+import './WeatherForecast.css'; 
 
 export const CurrentWeather: React.FunctionComponent<{ weather: Weather }> = ({ weather }) => {
 
@@ -13,7 +14,7 @@ export const CurrentWeather: React.FunctionComponent<{ weather: Weather }> = ({ 
 
     return (
 
-        <div className='bg-light'>
+        <div className='bg-light add'>
             <div className="row">
                 <div className='col-md-6'>
                     <h4 className='m-3 text-primary'><b>CURRENT WEATHER</b></h4>
