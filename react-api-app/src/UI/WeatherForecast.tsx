3427a08b3d75b1e1f1b78d7ecedda33e4dd090c9
Valery <pritulyk2@mail.ru>
2025-12-11 22:27:48 +0300

    Add CSS

--- react-api-app/src/UI/WeatherForecast.tsx
+++ react-api-app/src/UI/WeatherForecast.tsx
@@ -14,13 +14,26 @@ export const CurrentWeather: React.FunctionComponent<{ weather: Weather }> = ({ 
 
     return (
 
+       <body>
         <div className='bg-light add'>
             <div className="row">
                 <div className='col-md-6'>
-                    <h4 className='m-3 text-primary'><b>CURRENT WEATHER</b></h4>
+                    <section className="wather-forecast">
+                    <h1 className='m-3 text-primary'><b>Прогноз погоды</b></h1>
+                    <h2 className='m-3 text-primary weather-info'><p>Дата {dateNow}</p></h2>
+                    <h4 className='m-3 text-primary weather-info'><p>Рассвет с {sunrise}</p></h4>
+                    <h4 className='m-3 text-primary weather-info'><p>Закат с {sunset}</p></h4>
+                    <h4 className='m-3 text-primary city'><p>Город {weather.cityName}</p></h4>
+                    
+                    <h4 className='m-3 text-primary temperature-box'><p>Температура {weather.realFeel}</p></h4>
+                    <h4 className='m-3 text-primary feels-like-box'><p> Ощущается {weather.temperatureCelsius}</p></h4>
+                   <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="WeatherIcon" />
+                    <h4 className='m-3 text-primary'><p>Состояние {weather.weatherConditions}</p></h4>
+                    </section>
                 </div>
 
             </div>
         </div>
-    )
-}
+        </body>
+
+    )}
